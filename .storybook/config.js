807a5615e1b3d72fe382ea0c2207057c5b1f3892
youchao.liu <liuyouchao111@gmail.com>
2018-05-06 23:54:49 +0800

    update `storybook`

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,8 +1,12 @@
-import { configure } from '@storybook/react';
+import { configure, addDecorator } from '@storybook/react';
 import '@storybook/addon-console';
+import { withNotes } from '@storybook/addon-notes';
+import { withConsole } from '@storybook/addon-console';
 
 function loadStories() {
   require('../stories/index.js');
 }
 
+addDecorator(withNotes);
+addDecorator((storyFn, context) => withConsole()(storyFn)(context));
 configure(loadStories, module);
--- stories/index.js
+++ stories/index.js
@@ -6,27 +6,139 @@ import WithWorkersTemplate from './components/WithWorkersTemplate';
 storiesOf('Cronr', module)
   .add('Trigger on every 50 milliseconds', () => {
     return <Template pattern="*/50 * * * * * *" />;
+  }, {
+    notes: `
+      <b>Open Dev tools to check log detail</b>
+<pre>
+import CronrWorker from 'cronr'
+
+const job = new Cronr("*/50 * * * * * *", () => {
+    const { count } = this.state;
+  this.setState({
+    count: count + 1,
+  });
+});
+</pre>
+    `
   })
   .add('Trigger on every second', () => {
     return <Template pattern="*/1 * * * * *" />;
+  }, {
+    notes: `
+      <b>Open Dev tools to check log detail</b>
+<pre>
+import CronrWorker from 'cronr'
+
+const job = new Cronr("*/1 * * * * *", () => {
+    const { count } = this.state;
+  this.setState({
+    count: count + 1,
+  });
+});
+</pre>
+    `
   })
   .add('Trigger on 2,5 second', () => {
     return <Template pattern="2,5 * * * * *" />;
+  }, {
+    notes: `
+      <b>Open Dev tools to check log detail</b>
+<pre>
+import CronrWorker from 'cronr'
+
+const job = new Cronr("2,5 * * * * *", () => {
+    const { count } = this.state;
+  this.setState({
+    count: count + 1,
+  });
+});
+</pre>
+    `
   })
   .add('Trigger on 7,8,9,10 second', () => {
     return <Template pattern="7-10 * * * * *" />;
+  }, {
+    notes: `
+    <b>Open Dev tools to check log detail</b>
+<pre>
+import CronrWorker from 'cronr'
+
+const job = new Cronr("7-10 * * * * *", () => {
+  const { count } = this.state;
+this.setState({
+  count: count + 1,
+});
+});
+</pre>
+  `
   });
 
 storiesOf('with webWorkers', module)
   .add('Trigger on every 50 milliseconds', () => {
     return <WithWorkersTemplate pattern="*/50 * * * * * *" />;
+  }, {
+    notes:`
+    <b>Open Dev tools to check log detail</b>
+<pre>
+import CronrWorker from 'cronr/CronrWorker'
+
+const worker = CronrWorker("*/50 * * * * * *");
+worker.onmessage = (e) => {
+  if (action === 'trigger') {
+    // ...
+  }
+};
+</pre>
+`
   })
   .add('Trigger on every second', () => {
     return <WithWorkersTemplate pattern="*/1 * * * * *" />;
+  }, {
+    notes:`
+    <b>Open Dev tools to check log detail</b>
+<pre>
+import CronrWorker from 'cronr/CronrWorker'
+
+const worker = CronrWorker("*/1 * * * * *");
+worker.onmessage = (e) => {
+  if (action === 'trigger') {
+    // ...
+  }
+};
+</pre>
+`
   })
   .add('Trigger on 2,5 second', () => {
     return <WithWorkersTemplate pattern="2,5 * * * * *" />;
+  }, {
+    notes:`
+    <b>Open Dev tools to check log detail</b>
+<pre>
+import CronrWorker from 'cronr/CronrWorker'
+
+const worker = CronrWorker("2,5 * * * * *");
+worker.onmessage = (e) => {
+  if (action === 'trigger') {
+    // ...
+  }
+};
+</pre>
+`
   })
   .add('Trigger on 7,8,9,10 second', () => {
     return <WithWorkersTemplate pattern="7-10 * * * * *" />;
+  }, {
+    notes:`
+    <b>Open Dev tools to check log detail</b>
+<pre>
+import CronrWorker from 'cronr/CronrWorker'
+
+const worker = CronrWorker("7-10 * * * * *");
+worker.onmessage = (e) => {
+  if (action === 'trigger') {
+    // ...
+  }
+};
+</pre>
+`
   });
